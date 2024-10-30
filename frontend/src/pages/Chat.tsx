a3deb92dfbecd58eee9799687862650080235c94
Dibakar Ghosh <ghoshdibakar81@gmail.com>
2024-10-30 10:10:44 +0530

    added Home, Chat, Groups pages

---
+++ frontend/src/pages/Chat.tsx
@@ -0,0 +1,10 @@
+import AppLayout from "@/components/Layout/AppLayout"
+
+
+const Chat = () => {
+  return (
+    <div>Chat</div>
+  )
+}
+
+export default AppLayout()(Chat)
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import AppLayout from "@/components/Layout/AppLayout"
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default AppLayout()(Home)
