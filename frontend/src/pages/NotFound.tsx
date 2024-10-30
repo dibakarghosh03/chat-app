d2130ad0ee11f99c46685a979a1a752e1d60d82b
Dibakar Ghosh <ghoshdibakar81@gmail.com>
2024-10-30 10:10:56 +0530

    created NotFound page

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center text-4xl font-semibold'>NotFound</div>
+  )
+}
+
+export default NotFound
